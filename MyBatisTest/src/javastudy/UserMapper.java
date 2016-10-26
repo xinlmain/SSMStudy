221424213ce2e0b062808602f99e8aaebdce5b62
Xin Liang <xinlmain@163.com>
2016-10-26 23:51:26 +0800

    lesson 5 - 把sql命令放在mapper注解中
    1. 使用注解
    2. 把原来的mapper xml内容删除，否则会冲突

--- MyBatisTest/src/javastudy/UserMapper.java
+++ MyBatisTest/src/javastudy/UserMapper.java
@@ -2,10 +2,24 @@ package javastudy;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
 public interface UserMapper {
+	@Select("select * from User where id = #{id}")
 	public User selectUser(int id);
+	
+	@Select("select * from User")
 	public List<User> selectAll();
+	
+	@Insert("insert into User (userName,userPwd) values (#{userName},#{userPwd})")
 	public void addUser(User user);
+	
+	@Update("update User set userName=#{userName}, userPwd=#{userPwd} where id=#{id}")
 	public void updateUser(User user);
+	
+	@Delete("delete from User where id=#{id}")
 	public void deleteUser(int id);
 }
