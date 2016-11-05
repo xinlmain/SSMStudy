a23e7a1a5e469c02fa77379993783cc9c7cdcd47
Xin Liang <xinlmain@163.com>
2016-11-05 18:50:54 +0800

    把controller参数改为domain class

--- BookSystem/src/controller/UserController.java
+++ BookSystem/src/controller/UserController.java
@@ -5,14 +5,16 @@ import javax.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import entity.User;
+
 @Controller
 public class UserController {
 	
 	 @RequestMapping("/check.do")
-	public String check(HttpSession session, String name, String pwd) {
+	public String check(HttpSession session, User user) {
 		String path = "login.jsp";
-		if ("zhang".equals(name)) {
-			session.setAttribute("name", name);
+		if ("zhang".equals(user.getName())) {
+			session.setAttribute("name", user.getName());
 			path = "welcome.jsp";
 		}
 		return "redirect:" + path;
